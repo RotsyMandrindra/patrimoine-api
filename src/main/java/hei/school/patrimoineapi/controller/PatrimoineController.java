7c4194f6794c70e1107d76f645c8b571b260d066
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-09-23 16:16:09 +0300

    fix: change the getPatrimoine and createOrUpdatePatrimoine return value to ResponseEntity<Patrimoine>

--- src/main/java/hei/school/patrimoineapi/controller/PatrimoineController.java
+++ src/main/java/hei/school/patrimoineapi/controller/PatrimoineController.java
@@ -4,6 +4,8 @@ import hei.school.patrimoineapi.model.Patrimoine;
 import hei.school.patrimoineapi.service.PatrimoineService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -15,12 +17,23 @@ public class PatrimoineController {
     private PatrimoineService patrimoineService;
 
     @PutMapping("/id")
-    public Patrimoine createOrUpdatePatrimoine(@RequestBody Patrimoine patrimoine) throws IOException {
-        return patrimoineService.createOrUpdatePatrimoine(patrimoine);
+    public ResponseEntity<Patrimoine> createOrUpdatePatrimoine(@RequestBody Patrimoine patrimoine) throws IOException {
+        try {
+            Patrimoine updatedPatrimoine = patrimoineService.createOrUpdatePatrimoine(patrimoine);
+            return ResponseEntity.ok(updatedPatrimoine);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
     }
 
     @GetMapping("/id")
-    public Patrimoine getPatrimoine(@PathVariable int id) throws IOException {
-        return patrimoineService.getPatrimoine(id);
+    public ResponseEntity<Patrimoine> getPatrimoine(@PathVariable int id) throws IOException {
+        Patrimoine patrimoine = patrimoineService.getPatrimoine(id);
+        if (patrimoine != null) {
+            return ResponseEntity.ok(patrimoine);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
