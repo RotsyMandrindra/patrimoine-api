82554572e846f465b6466a9d88a9f9082ce659b7
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2024-09-23 14:12:27 +0300

    init an empty spring boot project

---
+++ src/test/java/hei/school/patrimoineapi/PatrimoineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.patrimoineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
